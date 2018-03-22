175483c3590ff33a0ebb1ac483f0ba571efa00aa
Arkadiusz Kalita <w57448@student.wsiz.rzeszow.pl>
2018-03-22 18:45:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/stud/myapplication/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.stud.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    RadioButton rbOpcja1;
+    RadioButton rbOpcja2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rbOpcja1 = findViewById(R.id.rbOpcja1);
+        rbOpcja1 = findViewById(R.id.rbOpcja2);
+
+        View.OnClickListener Listner = new View.OnClickListener(){
+
+            @Override
+            public void onClick (View view){
+                RadioButton radio = (RadioButton)
+                        String wartosc = "";
+
+                RadioButton RADIO = (RadioButton) view;
+                Toast.makeText(  MainActivity.this, text: "Kliknięto przycisk", radio.getText(), Toast.LENGTH_SHORT).show();
+                if (view.getId() == R.id.rbOpcja1){}
+                else if (view.getId() == R.id.rbOpcja2){}
+
+
+            }
+
+        };
+        rbOpcja1.setOnClickListener(Listner);
+        rbOpcja2.setOnClickListener(Listner);
+    }
+}
